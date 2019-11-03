088fd4e309fb8c61571fb43f755c5804bf84cbb5
kaihoyu <kyho.yu@gmail.com>
2019-11-03 21:59:17 +0100

    01_update

---
+++ 01_AgentPartol/Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float z;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(x, y, z, Space.Self);
+    }
+}
