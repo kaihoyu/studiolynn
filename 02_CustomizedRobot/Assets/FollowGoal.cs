83146039f0cb1d385f4af711195108cde106a049
kaihoyu <kyho.yu@gmail.com>
2019-11-04 17:56:58 +0100

    02_update

---
+++ 02_CustomizedRobot/Assets/FollowGoal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowGoal : MonoBehaviour {
+
+	public Transform goal;
+	float speed = 2.0f;
+	float accuracy = 1.0f;
+	float rotSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 lookAtGoal = new Vector3(goal.position.x, 
+										this.transform.position.y, 
+										goal.position.z);
+		Vector3 direction = lookAtGoal - this.transform.position;
+
+		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+												Quaternion.LookRotation(direction), 
+												Time.deltaTime*rotSpeed);
+
+		this.transform.Translate(0,0,speed*Time.deltaTime);
+	}
+}
---
+++ 02_CustomizedRobot/Assets/NewCarAutoMove.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(NewCarController))]
+public class NewCarAutoMove : MonoBehaviour
+{
+    private NewCarController m_Car; // the car controller we want to use
+    public Transform goal;
+
+
+    private void Awake()
+    {
+        // get the car controller
+        m_Car = GetComponent<NewCarController>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        var dir = goal.position - m_Car.transform.position;
+        var dirH = Vector3.Project(dir, m_Car.transform.right);
+
+        var v = 0f;
+        if (dir.magnitude > 5f)
+        {
+            v = 3;
+        }
+        var h = 0f;
+
+        if (Vector3.Angle(dir,m_Car.transform.forward)<15)
+        {         
+            v = 3;        
+        }
+        else
+        {          
+            Debug.Log(dirH);
+            if ((m_Car.transform.right + dirH.normalized/2).magnitude > 1)
+            {
+                h = 3;
+            }
+            else
+            {
+                h = -3;
+            }
+            //h = -3;
+        }
+
+        var handbrake = 0f;
+        //if(dir.magnitude < 2f)
+        //{
+        //    handbrake = 1;
+        //}
+
+
+        m_Car.Move(h,v,v,handbrake);
+
+    }
+}
